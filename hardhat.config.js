b73c3789b68e51196a4a65bc1e6a786e1393f5f9
Кирилл Дудников <kddudnikov@edu.hse.ru>
2023-04-07 04:15:52 +0300

    Editing .env files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: `.env.private` });
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
 
@@ -6,7 +7,11 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
-    }
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+    },
   },
   solidity: {
     version: "0.8.18",
--- scripts/initTransferTokens.js
+++ scripts/initTransferTokens.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: `.env.public` });
 
 async function main() {
   const fs = require('fs');
