dc9fa7d0b4568af655358071af5f43b85b4936e4
Кирилл Дудников <kddudnikov@edu.hse.ru>
2023-04-08 05:13:00 +0300

    Error handling try 4

--- front/src/components/ErrorBoundary.tsx
+++ front/src/components/ErrorBoundary.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Box, Typography } from '@mui/material';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -17,15 +18,21 @@ class ErrorBoundary extends React.Component {
   render() {
     if (this.state.hasError) {
       return (
-        <div>
-          <h2>Oops, there is an error!</h2>
-          <button
-            type="button"
-            onClick={() => this.setState({ hasError: false })}
-          >
-            Try again?
-          </button>
-        </div>
+        <Box sx={{ mt: 10, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Box>
+            <Typography variant="h6" sx={{ fontWeight: 'bold', transform: 'rotate(-3deg)' }}>Oops, looks like you have problems with Metamask!</Typography>
+            <Typography>
+              <Box sx={{ transform: 'rotate(-2deg)', mt: 3 }}>1. Metamask browser extension must be installed.</Box>
+              <Box sx={{ transform: 'rotate(-1deg)', mt: 1 }}>2. The correct network must be selected.</Box>
+              <Box sx={{ transform: 'rotate(-1deg)', ml: 6 }}>2.1. For the online version it is testnet Sepolia.</Box>
+              <Box sx={{ transform: 'rotate(-1deg)', ml: 6 }}>2.1. For the local version it is localhost:8545.</Box>
+            </Typography>
+            <Button sx={{ mt: 5, ml: 20, borderRadius: 0, color: 'red', borderColor: 'black', transform: 'rotate(-10deg)' }}
+              variant='outlined' onClick={() => this.setState({ hasError: false })}>
+              Try again?
+            </Button>
+          </Box>
+        </Box>
       )
     }
 
